7d0d389997c5719982a357a6c094a6a749d7f10d
ZhengLiJing <987906521@qq.com>
2019-03-08 15:58:23 +0800

    change the paginate function to Paginator constructor, meanwhile update the test

--- src/jk-paginate.ts
+++ src/jk-paginate.ts
@@ -1,71 +1,78 @@
-function paginate (options) {
-  let totalItems: number = options.totalItems
-  let currentPage: number = options.currentPage || 1
-  let pageSize: number = options.pageSize || 10
-  let pageLinks: number = options.pageLinks || 10
+var Paginator = function(options) {
+  let defaultOption = {
+    totalItems: 100,
+    currentPage: 1,
+    pageSize: 10,
+    pageLinks: 10
+  };
+  this.options = Object.assign({}, defaultOption, options);
+};
 
-  // 计算总页数，比如一共300个条目，每页显示的条目 = 总条目/pageSize
-  let totalPages = Math.ceil(totalItems / pageSize)
+Paginator.prototype = {
+  calc: function() {
+    let { totalItems, currentPage, pageSize, pageLinks } = this.options;
+    // 计算总页数，比如一共300个条目，每页显示的条目 = 总条目/pageSize
+    let totalPages = Math.ceil(totalItems / pageSize);
 
-  let oddPageLinks = (pageLinks % 2 !== 0) ? 0 : 1
+    let oddPageLinks = pageLinks % 2 !== 0 ? 0 : 1;
 
-  // 确保当前页不越界
-  if (currentPage < 1) {
-    currentPage = 1
-  } else if (currentPage > totalPages) {
-    currentPage = totalPages
-  }
-  let startPage: number, endPage: number
-  // 如果总页数小于最大页数，比如总页数8也，pageLinks=10,则全部全部页数 
-  if (totalPages < pageLinks) {
-    startPage = 1
-    endPage = totalPages
-  } else {
-    // 如果总页数大于最大页数，比如总页数20也，pageLinks=10，
-    // 则需要计算起始页和末尾页，有三种情况
-
-    // 当前页之前最多能显示的页数
-    let maxPagesBeforeCurrentPage = Math.floor(pageLinks / 2)
-    // 当前页之后最多能显示的页数
-    let maxPagesAfterCurrentPage = Math.floor(pageLinks / 2) - oddPageLinks
-    // 情况一：当前页靠近首页
-    if (currentPage <= maxPagesBeforeCurrentPage) {
-      startPage = 1
-      endPage = pageLinks
-      // (totalItems - maxPagesAfterCurrentPage) < currentPage && currentPage < totalItems
-    } else if (currentPage + maxPagesAfterCurrentPage > totalPages) {
-      // 情况二：当前页靠近末页
-      
-      startPage = totalPages - pageLinks + 1
-      endPage = totalPages
+    // 确保当前页不越界
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > totalPages) {
+      currentPage = totalPages;
+    }
+    let startPage: number, endPage: number;
+    // 如果总页数小于最大页数，比如总页数8也，pageLinks=10,则全部全部页数
+    if (totalPages < pageLinks) {
+      startPage = 1;
+      endPage = totalPages;
     } else {
-      // 情况三：当前页位于中间
-      startPage = currentPage - maxPagesBeforeCurrentPage
-      endPage = currentPage + maxPagesAfterCurrentPage
-    }
-    // 计算条目的起始和末尾索引
-    let startIndex = (currentPage - 1) * pageSize 
-    let endIndex = Math.min(startIndex + pageSize - 1, totalItems)
+      // 如果总页数大于最大页数，比如总页数20也，pageLinks=10，
+      // 则需要计算起始页和末尾页，有三种情况
 
-    // 创建一个页面数组，[1,2,3,4,5,6,7,8,9,10]
-    let pages = Array
-      .from(Array((endPage + 1) - startPage)
-      .keys())
-      .map(i => startPage + i)
-    
-    // 返回一个包含所有信息的对象
-    return {
-      totalItems,
-      currentPage,
-      pageSize,
-      totalPages,
-      startPage,
-      endPage,
-      startIndex,
-      endIndex,
-      pages
+      // 当前页之前最多能显示的页数
+      let maxPagesBeforeCurrentPage = Math.floor(pageLinks / 2);
+      // 当前页之后最多能显示的页数
+      let maxPagesAfterCurrentPage = Math.floor(pageLinks / 2) - oddPageLinks;
+      // 情况一：当前页靠近首页
+      if (currentPage <= maxPagesBeforeCurrentPage) {
+        startPage = 1;
+        endPage = pageLinks;
+        // (totalItems - maxPagesAfterCurrentPage) < currentPage && currentPage < totalItems
+      } else if (currentPage + maxPagesAfterCurrentPage > totalPages) {
+        // 情况二：当前页靠近末页
+
+        startPage = totalPages - pageLinks + 1;
+        endPage = totalPages;
+      } else {
+        // 情况三：当前页位于中间
+        startPage = currentPage - maxPagesBeforeCurrentPage;
+        endPage = currentPage + maxPagesAfterCurrentPage;
+      }
+      // 计算条目的起始和末尾索引
+      let startIndex = (currentPage - 1) * pageSize;
+      let endIndex = Math.min(startIndex + pageSize - 1, totalItems);
+
+      // 创建一个页面数组，[1,2,3,4,5,6,7,8,9,10]
+      let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+        i => startPage + i
+      );
+
+      // 返回一个包含所有信息的对象
+      return {
+        totalItems,
+        currentPage,
+        pageSize,
+        totalPages,
+        startPage,
+        endPage,
+        startIndex,
+        endIndex,
+        pages
+      };
     }
   }
-}
+};
 
-export = paginate
+export { Paginator };
--- test/paginate.js
+++ test/paginate.js
@@ -1,128 +1,145 @@
-const assert = require('assert')
-const paginate = require('../lib/jk-paginate.js')
+const assert = require("assert");
+const Paginator = require("../lib/jk-paginate.js").Paginator;
 
-function test () {
-  console.log('Running Paginate test')
-  var item = {
+function test() {
+  console.log("Running Paginate test");
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 1
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 1,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 1,
-    endPage: 10,
-    startIndex: 0,
-    endIndex: 9,
-    pages: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-  }, paginate(item))
-  
-  var item = {
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 1,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 1,
+      endPage: 10,
+      startIndex: 0,
+      endIndex: 9,
+      pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    item.calc()
+  );
+
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 8
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 8,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 1,
-    endPage: 10,
-    startIndex: 70,
-    endIndex: 79,
-    pages: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-  }, paginate(item))
-
-  var item = {
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 8,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 1,
+      endPage: 10,
+      startIndex: 70,
+      endIndex: 79,
+      pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    item.calc()
+  );
+  
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 11,
     pageLinks: 7
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 10,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 4,
-    endPage: 10,
-    startIndex: 90,
-    endIndex: 99,
-    pages: [ 4, 5, 6, 7, 8, 9, 10 ]
-  }, paginate(item))
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 10,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 4,
+      endPage: 10,
+      startIndex: 90,
+      endIndex: 99,
+      pages: [4, 5, 6, 7, 8, 9, 10]
+    },
+    item.calc()
+  );
 
-
-  var item = {
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 8,
     pageLinks: 7
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 8,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 4,
-    endPage: 10,
-    startIndex: 70,
-    endIndex: 79,
-    pages: [4, 5, 6, 7, 8, 9, 10 ]
-  }, paginate(item))
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 8,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 4,
+      endPage: 10,
+      startIndex: 70,
+      endIndex: 79,
+      pages: [4, 5, 6, 7, 8, 9, 10]
+    },
+    item.calc()
+  );
 
-  var item = {
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 5,
     pageLinks: 7
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 5,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 2,
-    endPage: 8,
-    startIndex: 40,
-    endIndex: 49,
-    pages: [ 2, 3, 4, 5, 6, 7, 8 ]
-  }, paginate(item))
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 5,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 2,
+      endPage: 8,
+      startIndex: 40,
+      endIndex: 49,
+      pages: [2, 3, 4, 5, 6, 7, 8]
+    },
+    item.calc()
+  );
 
-  var item = {
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 5,
     pageLinks: 6
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 5,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 2,
-    endPage: 7,
-    startIndex: 40,
-    endIndex: 49,
-    pages: [ 2, 3, 4, 5, 6, 7 ]
-  }, paginate(item))
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 5,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 2,
+      endPage: 7,
+      startIndex: 40,
+      endIndex: 49,
+      pages: [2, 3, 4, 5, 6, 7]
+    },
+    item.calc()
+  );
 
-
-  var item = {
+  var item = new Paginator({
     totalItems: 100,
     currentPage: 5,
     pageLinks: 4
-  }
-  assert.deepEqual({
-    totalItems: 100,
-    currentPage: 5,
-    pageSize: 10,
-    totalPages: 10,
-    startPage: 3,
-    endPage: 6,
-    startIndex: 40,
-    endIndex: 49,
-    pages: [ 3, 4, 5, 6 ]
-  }, paginate(item))
-
-  
+  });
+  assert.deepEqual(
+    {
+      totalItems: 100,
+      currentPage: 5,
+      pageSize: 10,
+      totalPages: 10,
+      startPage: 3,
+      endPage: 6,
+      startIndex: 40,
+      endIndex: 49,
+      pages: [3, 4, 5, 6]
+    },
+    item.calc()
+  );
 }
 
-test()
+test();
