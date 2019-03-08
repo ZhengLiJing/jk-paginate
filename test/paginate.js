0328336e191764c66caf0a9c234873b2b6369c8f
ZhengLiJing <987906521@qq.com>
2019-03-08 14:36:16 +0800

    Adding paginate.js test

---
+++ test/paginate.js
@@ -0,0 +1,128 @@
+const assert = require('assert')
+const paginate = require('../lib/jk-paginate.js')
+
+function test () {
+  console.log('Running Paginate test')
+  var item = {
+    totalItems: 100,
+    currentPage: 1
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 1,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 1,
+    endPage: 10,
+    startIndex: 0,
+    endIndex: 9,
+    pages: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  }, paginate(item))
+  
+  var item = {
+    totalItems: 100,
+    currentPage: 8
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 8,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 1,
+    endPage: 10,
+    startIndex: 70,
+    endIndex: 79,
+    pages: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  }, paginate(item))
+
+  var item = {
+    totalItems: 100,
+    currentPage: 11,
+    pageLinks: 7
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 10,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 4,
+    endPage: 10,
+    startIndex: 90,
+    endIndex: 99,
+    pages: [ 4, 5, 6, 7, 8, 9, 10 ]
+  }, paginate(item))
+
+
+  var item = {
+    totalItems: 100,
+    currentPage: 8,
+    pageLinks: 7
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 8,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 4,
+    endPage: 10,
+    startIndex: 70,
+    endIndex: 79,
+    pages: [4, 5, 6, 7, 8, 9, 10 ]
+  }, paginate(item))
+
+  var item = {
+    totalItems: 100,
+    currentPage: 5,
+    pageLinks: 7
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 5,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 2,
+    endPage: 8,
+    startIndex: 40,
+    endIndex: 49,
+    pages: [ 2, 3, 4, 5, 6, 7, 8 ]
+  }, paginate(item))
+
+  var item = {
+    totalItems: 100,
+    currentPage: 5,
+    pageLinks: 6
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 5,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 2,
+    endPage: 7,
+    startIndex: 40,
+    endIndex: 49,
+    pages: [ 2, 3, 4, 5, 6, 7 ]
+  }, paginate(item))
+
+
+  var item = {
+    totalItems: 100,
+    currentPage: 5,
+    pageLinks: 4
+  }
+  assert.deepEqual({
+    totalItems: 100,
+    currentPage: 5,
+    pageSize: 10,
+    totalPages: 10,
+    startPage: 3,
+    endPage: 6,
+    startIndex: 40,
+    endIndex: 49,
+    pages: [ 3, 4, 5, 6 ]
+  }, paginate(item))
+
+  
+}
+
+test()
